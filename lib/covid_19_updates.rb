55ae2b5cc075b023459c5c966c903d7ecbc4348c
Jack Madden <jck13mad@gmail.com>
2020-04-25 15:12:27 -0400

    Add basic command for greeting

--- lib/covid_19_updates.rb
+++ lib/covid_19_updates.rb
@@ -1,17 +1,16 @@
 require "covid_19_updates/version"
 require "covid_19_updates/cli"
-require "thor"
+
 
 module Covid19Updates
-  class CLI < Thor
-    desc "greet NAME", "say hello to NAME"
-    def greet(name)
-      puts "Hello #{name}"
-    end
-  end
+  # class CLI < Thor
+  #   desc "greet NAME", "say hello to NAME"
+  #   def greet(name)
+  #     puts "Hello #{name}"
+  #   end
+  # end
 
-  class Testing
-  end
+
 end
 
 
--- lib/covid_19_updates/cli.rb
+++ lib/covid_19_updates/cli.rb
@@ -1,11 +1,28 @@
 
+require "thor"
+
+module Covid19Updates
+  class CLI < Thor
+    desc 'start', 'Prints out greeting and basic commands"'
+    start
+
+    private
+
+    def start
+      greeting = <<~DOC
+    
+      Welcome to Covid-19 Updates!
+      View the top headlines from the following sites: 
+      
+        1. CNBC
+        2. FOXNEWS
+        3. CNN
+        4. APPLE
+      DOC
+
+      puts greeting
+    end
 
 
-# module Covid19Updates
-#   class CLI < Thor
-#     desc "greet NAME", "say hello to NAME"
-#     def greet(name)
-#       puts "Hello #{name}"
-#     end
-#   end
-# end
+  end
+end

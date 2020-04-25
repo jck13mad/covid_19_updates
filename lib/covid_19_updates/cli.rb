5a9d8e539a1eba24d0475e849afbdb953ca6e5b2
Jack Madden <jck13mad@gmail.com>
2020-04-25 14:39:15 -0400

    Extract CLI into Covid19Updates namespace

--- lib/covid_19_updates/cli.rb
+++ lib/covid_19_updates/cli.rb
@@ -1,6 +1,11 @@
-class CLI < Thor
-  desc "greet NAME", "say hello to NAME"
-  def greet(name)
-    puts "Hello #{name}"
+
+
+
+module Covid19Updates
+  class CLI < Thor
+    desc "greet NAME", "say hello to NAME"
+    def greet(name)
+      puts "Hello #{name}"
+    end
   end
 end

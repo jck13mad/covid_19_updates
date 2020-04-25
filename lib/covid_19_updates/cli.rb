725e39ab2477e5d6b063e576a8d1115cd25a75c6
Jack Madden <jck13mad@gmail.com>
2020-04-25 15:32:29 -0400

    Refactor

--- lib/covid_19_updates/cli.rb
+++ lib/covid_19_updates/cli.rb
@@ -15,7 +15,7 @@ module Covid19Updates
 
     desc 'start', 'Prints out greeting and basic commands'
     def list 
-      call
+      list_new_networks
     end
 
     private
@@ -34,6 +34,18 @@ module Covid19Updates
       Enter the number of news website you would like to see updates from.\n Please type list to see options or exit to leave.
       DOC
 
+      puts greeting
+    end
+
+    def list_new_networks
+      greeting = <<~DOC
+        1. CNBC
+        2. FOXNEWS
+        3. CNN
+        4. STATNEWS
+
+      Enter the number of news website you would like to see updates from.\n Please type list to see options or exit to leave.
+      DOC
       puts greeting
     end
 

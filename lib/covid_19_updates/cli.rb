e199ff52a075f1699a400022389450586279134a
Jack Madden <jck13mad@gmail.com>
2020-04-26 15:15:26 -0400

    Cnn headline

--- lib/covid_19_updates/cli.rb
+++ lib/covid_19_updates/cli.rb
@@ -27,6 +27,8 @@ module Covid19Updates
         cnbc_headline
       when '2'
         fox_headline
+      when '3'
+        cnn_headline
       else
         puts 'No news'
       end
@@ -49,7 +51,7 @@ module Covid19Updates
       end
 
       puts
-      pick = ask('Pick a news item')
+      pick = ask('Please pick a number to view an update: ')
       puts
 
       print Nokogiri::HTML(URI.open((array[pick.to_i - 1][:link]).to_s)).css('div.group').text.gsub('Â', '')
@@ -78,16 +80,43 @@ module Covid19Updates
       end
 
       puts
-        pick = ask('Pick a news item')
+        pick = ask('Please pick a number to view an update: ')
       puts
 
 
       # n.css('p')[rand(3..(n.css('p').count))].text (n.css('p').count)
       n = final[pick.to_i - 1][:link].to_s
+      node = Nokogiri::HTML(URI.open(n)).css('p')
 
-      print Nokogiri::HTML(URI.open(n)).css('p')[rand(3..(Nokogiri::HTML(URI.open(n)).css('p').count))].text
+      print node[rand(3..(node.count))].text
       puts
     end
+
+    def cnn_headline
+      doc = Nokogiri::HTML(URI.open('https://www.cnn.com/world/live-news/coronavirus-pandemic-04-26-20-intl/index.html'))
+      items = doc.css('h2.post-headlinestyles_Headline-sc-2ts3cz-1 gzgZ0i')
+      
+      array = []
+      items.each do |item|
+        array << { title: item.text }
+      end
+
+      rray.each_with_index do |news, index|
+        puts "#{index.succ}: #{news[:title]}"
+      end
+
+      puts
+        pick = ask('Please pick a number to view an update: ')
+      puts 
+
+      
+
+
+
+
+
+    end
+
 
     def call
       greeting = <<~DOC
@@ -95,9 +124,9 @@ module Covid19Updates
         View the top headlines from the following sites:
 
           1. CNBC
-          2. FOXNEWS
+          2. FOX NEWS
           3. CNN
-          4. STATNEWS
+          4. STAT NEWS
 
         Enter the number of news website you would like to see updates from.\n Please type cov list to see options or exit to leave.
       DOC
@@ -108,9 +137,9 @@ module Covid19Updates
     def list_new_networks
       greeting = <<~DOC
           1. CNBC
-          2. FOXNEWS
+          2. FOX NEWS
           3. CNN
-          4. STATNEWS
+          4. STAT NEWS
 
         Enter the number of news website you would like to see updates from.\n Please type list to see options or exit to leave.
       DOC
